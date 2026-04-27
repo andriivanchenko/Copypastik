1e005eab0df6b7d5baa1c6da75808a0e5c7e8e8a
Andrii Ivanchenko <ivan4enko.andrey01@gmail.com>
2026-04-27 19:51:34 +0300

    Initial Commit

---
+++ PastePure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PastePure
+//
+//  Created by Andrii Ivanchenko on 27.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PastePure/PastePureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastePureApp.swift
+//  PastePure
+//
+//  Created by Andrii Ivanchenko on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PastePureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
